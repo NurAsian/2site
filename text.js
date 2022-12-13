8cca55182713d9875259b30ef84a4ee4bdcfdee0
NurAsian <74744849+NurAsian@users.noreply.github.com>
2022-12-13 17:38:29 +0700

    Update text.js

--- text.js
+++ text.js
@@ -1,6 +1,7 @@
-<h2>display: contents:</h2>
-<div class="a">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. <div class="b">HELLO WORLD!</div> Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut.
-</div>
-
-<p>display: contents does not work in Edge, prior version 79.</p>
+document.write('<h2><?!=color_button ?></h2>');
+document.write('<h2>display: contents:</h2>');
+document.write('<div class="a">');
+document.write('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. <div class="b">HELLO WORLD!</div> Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut.');
+document.write('</div>');
+document.write('');
+document.write('<p>display: contents does not work in Edge, prior version 79.</p>');
